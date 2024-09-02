692f5c2522cb00166b20b2cbfcd148a4f1b7c613
djurke23 <106180427+djurke23@users.noreply.github.com>
2024-09-03 00:14:32 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// faq
+
+
+document.querySelectorAll('.faq-question').forEach(question => {
+    question.addEventListener('click', () => {
+        const item = question.parentNode;
+        item.classList.toggle('active');
+    });
+});
+
+
+
+
+
