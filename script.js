a616bc413d20cd41a1761208350662e1bfd73b03
djurke23 <106180427+djurke23@users.noreply.github.com>
2024-09-03 12:24:17 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,57 @@
+
+
+// o nama v2
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const services = document.querySelectorAll('.service');
+    services.forEach(service => {
+        service.addEventListener('mouseover', () => {
+            service.style.transform = 'translateX(10px)';
+        });
+        service.addEventListener('mouseout', () => {
+            service.style.transform = 'translateX(0)';
+        });
+    });
+});
+
+
+
+
+
+
+// CENE
+
+// Dodavanje jednostavne animacije kada kartice postanu vidljive
+
+window.addEventListener('scroll', () => {
+    const cards = document.querySelectorAll('.card');
+    const screenPosition = window.innerHeight / 1.3;
+
+    cards.forEach(card => {
+        const cardPosition = card.getBoundingClientRect().top;
+
+        if (cardPosition < screenPosition) {
+            card.classList.add('appear');
+        } else {
+            card.classList.remove('appear');
+        }
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
 // faq
 
 
